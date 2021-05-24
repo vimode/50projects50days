83292c3ac4be36cc74209dedaf4551e07fcc76cd
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-05-24 14:54:27 +0530

    Added project : hidden-searc

---
+++ hidden-search/script.js
@@ -0,0 +1,7 @@
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input_search')
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('open');
+})
